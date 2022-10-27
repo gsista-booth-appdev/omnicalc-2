ab9acf09a958d016aa6848aec50fdf5c13509410
gsista-booth <gsista@chicagobooth.edu>
2022-10-27 05:51:50 +0000

    Wizard Add working!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
+  def add
+    render({ :template => "calculator/add.html.erb" })
+  end
 
+  def wizard_add
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num + @second_num
+    render({ :template => "calculator/wizard_add.html.erb" })
+  end
+
+  def subtract
+    render({ :template => "calculator/subtract.html.erb" })
+  end
+
+  def multiply
+    render({ :template => "calculator/multiply.html.erb" })
+  end
+
+  def divide
+    render({ :template => "calculator/divide.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get("/add", { :controller => "application", :action => "add" })
 
+  get("/wizard_add", { :controller => "application", :action => "wizard_add" })
+
+  get("/subtract", { :controller => "application", :action => "subtract" })
+
+  get("/multiply", { :controller => "application", :action => "multiply" })
+
+  get("/divide", { :controller => "application", :action => "divide" })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
